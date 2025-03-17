b57db4eeb81c46fc388584c38173adc9c2329c53
ShinySaana <shinysaana@gmail.com>
2025-03-17 07:16:00 +0100

    fix: musicbrainz helper script's argument passing

--- musicbrainz/set-musicbrainz-release-data.sh
+++ musicbrainz/set-musicbrainz-release-data.sh
@@ -8,4 +8,4 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 python3 -m venv $SCRIPT_DIR/venv
 source $SCRIPT_DIR/venv/bin/activate
 
-$SCRIPT_DIR/set-musicbrainz-release-data.py $@
+$SCRIPT_DIR/set-musicbrainz-release-data.py "$@"
